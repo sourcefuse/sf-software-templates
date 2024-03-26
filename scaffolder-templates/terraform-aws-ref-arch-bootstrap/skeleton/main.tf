0828f9112b8bdae80adc270305164835bc37e6cb
Vishwas Yogi <vishwas.yogi@sourcefuse.com>
2024-03-26 10:47:23 +0530

    update terraform-aws-ref-arch-bootstrap template

--- scaffolder-templates/terraform-aws-ref-arch-bootstrap/skeleton/main.tf
+++ scaffolder-templates/terraform-aws-ref-arch-bootstrap/skeleton/main.tf
@@ -1,5 +1,5 @@
 module "bootstrap" {
-  source        = "git@github.com:sourcefuse/terraform-module-aws-bootstrap?ref=1.0.0"
+  source        = "git::https://github.com/sourcefuse/terraform-module-aws-bootstrap?ref=1.0.0"
   bucket_name   = "${var.bucket_name}-${var.environment}"
   dynamodb_name = "${var.dynamodb_lock_table_name}_${var.environment}"
   tags          = var.tags
